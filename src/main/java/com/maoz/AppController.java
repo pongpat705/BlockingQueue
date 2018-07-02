55a972c63c653de17f8506663385f4799518c9e7
fluke <fhluky@gmail.com>
2018-07-02 22:37:43 +0700

    add thymeleaf

--- src/main/java/com/maoz/AppController.java
+++ src/main/java/com/maoz/AppController.java
@@ -1,14 +1,16 @@
 package com.maoz;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
 public class AppController {
 
-    @GetMapping("/block")
-    public String block(){
-
-        return "block";
+    @GetMapping("/app")
+    public String block(Model model, @RequestParam(name = "name", required = false, defaultValue = "World") String name){
+        model.addAttribute("name", name);
+        return "app";
     }
 }
